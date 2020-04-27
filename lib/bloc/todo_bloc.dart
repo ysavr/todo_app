e3cfd14deb3909e4613b11981130c1ab220cbd08
Yusuf Safrudin <y.safrudin666@gmail.com>
2020-04-27 15:47:38 +0700

    first commit

---
+++ lib/bloc/todo_bloc.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:todo_app/model/todo.dart';
+import 'package:todo_app/repository/todo_repository.dart';
+
+class TodoBloc {
+  final _todoRepository = TodoRepository();
+
+  final _todoController = StreamController<List<Todo>>.broadcast();
+
+  get todos => _todoController.stream;
+
+  TodoBloc() {
+    getTodos();
+  }
+
+  getTodos({String query}) async {
+    _todoController.sink.add(await _todoRepository.getAllTodos(query: query));
+  }
+
+  addTodo(Todo todo) async {
+    await _todoRepository.insertTodo(todo);
+    getTodos();
+  }
+
+  updateTodo(Todo todo) async {
+    await _todoRepository.updateTodo(todo);
+    getTodos();
+  }
+
+  deleteTodoById(int id) async {
+    _todoRepository.deleteTodoById(id);
+    getTodos();
+  }
+
+  dispose() {
+    _todoController.close();
+  }
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/ui/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Reactive Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        canvasColor: Colors.transparent,
+      ),
+      home: HomePage(title: 'My Todo List'),
+    );
+  }
+}
